d734b6b5161d83a5995fc47a85e3b46a69ce5079
Håkon Otneim <haakon.otneim@gmail.com>
2015-09-10 14:24:37 +0200

    Added and documented transLocal

--- R/transLocal.R
+++ R/transLocal.R
@@ -1,4 +1,4 @@
-
+                                                                                  
 #######################################################
 ##                                                   ##
 ##   TRANSFORM DATA AND GRID TO STANDARD NORMALITY   ##
@@ -6,26 +6,20 @@
 ##                                                   ##
 #######################################################
 
-##      Author:        Håkon Otneim
-##      Date:          01.12.2014
-##      Description:   Takes a data matrix (one observation per row) and transforms each
-##                     observation vector to standard normality using the logspline desnity
-##                     estimator by Stone, Hansen and Kooperberg (1997).
-##
-##      Arguments:     data:                 The data matrix
-##                     grid:                 Provide the grid if it should be transformed as well
-##                     return.normalizing.constants:
-##                                           Logical. If true, return the normalizing constants as well.
-##
-##      Value:         transformed.data:          The transformed data matrix.
-##                     transformed.grid:          The transformed grid.
-##                     normalizing.constants:     The normalizing constants for use in 'multiLocal'.
-##
-##      Necessary packages:
-##                     logspline
-##
-##      References:    Stone, Charles J., et al. "Polynomial splines and their tensor products in extended linear modeling: 1994 Wald memorial lecture."
-##                         The Annals of Statistics 25.4 (1997): 1371-1470.
+#' Transform the marginals of a multivariate data set to standard normality based
+#' on the logspline density estimator.
+#' @param data The data matrix, one row per observation.
+#' @param grid The grid if it should be transformed as well.
+#' @param return.normalizing.constants TRUE if the normalizing constants to be
+#' used in the LGDE should be returned as well.
+#' @return A list containing the transformed data ($transformed.data), the transformed
+#' grid if provided ($transformed.grid), and the normalizing constants if chosen
+#' ($normalizing.constants).
+#' @examples
+#' data <- cbind(rt(100, df = 10), rt(100, df = 10))
+#' grid <- matrix(c(-1, -1, 0, 0, 1, 1), ncol = 2, byrow = TRUE)
+#' 
+#' transLocal(data, grid, return.normalizing.constants = TRUE)
 
 transLocal <- function(data,
                        grid = NULL,
@@ -36,25 +30,36 @@ transLocal <- function(data,
     d <- dim(data)[2]
                        
     # Estimate the marginals using the logspline
-    estimate.marginal <- function(i) logspline(data[,i])
+    estimate.marginal <- function(i) logspline::logspline(data[,i])
     marginal.estimates <- lapply(X = as.list(1:d), FUN = estimate.marginal)
-            
-    transform.data <- function(i) qnorm(plogspline(data[,i], marginal.estimates[[i]]))
-    transformed.data <- matrix(unlist(lapply(X = as.list(1:d), FUN = transform.data)), ncol = d)
 
+    # Transform each observation vector
+    transform.data <- function(i) qnorm(logspline::plogspline(data[,i],
+                                                              marginal.estimates[[i]]))
+    transformed.data <- matrix(unlist(lapply(X = as.list(1:d),
+                                             FUN = transform.data)), ncol = d)
+
+    # The list to be returned
     ret = list(transformed.data = transformed.data)
     
     # Transform the grid points
     if(!is.null(grid)) {
-        transform.grid <- function(i) qnorm(plogspline(grid[,i], marginal.estimates[[i]]))
-        transformed.grid <- matrix(unlist(lapply(X = as.list(1:d), FUN = transform.grid)), ncol = d)
+        transform.grid <- function(i)
+            qnorm(logspline::plogspline(grid[,i], marginal.estimates[[i]]))
+        transformed.grid <- matrix(unlist(lapply(X = as.list(1:d),
+                                                 FUN = transform.grid)), ncol = d)
         ret$transformed.grid <- transformed.grid
     }
 
     # Calculate the normalizing constants
     if(return.normalizing.constants) {
-        calculate.normalizing.constants <- function(i) dlogspline(grid[,i], marginal.estimates[[i]])/dnorm(qnorm(plogspline(grid[,i], marginal.estimates[[i]])))
-        normalizing.constants <- matrix(unlist(lapply(X = as.list(1:d), FUN = calculate.normalizing.constants)), ncol = d)
+        calculate.normalizing.constants <- function(i) {
+            logspline::dlogspline(grid[,i], marginal.estimates[[i]])/
+                dnorm(qnorm(plogspline(grid[,i], marginal.estimates[[i]])))
+        }
+        normalizing.constants <- matrix(unlist(lapply(X = as.list(1:d),
+                                                      FUN = calculate.normalizing.constants)),
+                                        ncol = d)
         ret$normalizing.constants <- normalizing.constants
     }         
 
